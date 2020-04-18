61d218324f142c24b321c32dfb8e62b1a0b4ba33
Pancha27 <diliniliyanage96@gmail.com>
2020-04-19 04:30:22 +0530

    DB ErrorFixed

--- AppointmentService/src/model/Appointment.java
+++ AppointmentService/src/model/Appointment.java
@@ -14,7 +14,7 @@ public class Appointment {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			// Provide the correct details: DBServer/DBName, username, password
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/health_care", "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcare", "root", "");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
