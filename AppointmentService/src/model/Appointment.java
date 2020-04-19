5cd507dfddc5e994811a52b8f02f78da4ffb3c89
Pancha27 <diliniliyanage96@gmail.com>
2020-04-19 17:28:37 +0530

    Finished

--- AppointmentService/src/model/Appointment.java
+++ AppointmentService/src/model/Appointment.java
@@ -101,7 +101,7 @@ public class Appointment {
 			if (con == null)
 			{return "Error while connecting to the database for updating."; }
 			// create a prepared statement
-			String query = "UPDATE items SET appointmentDate=?,appointmentTime=?,appointmentDoctor=?,appointmentHospital=?WHERE appointmentID=?";
+			String query = "UPDATE appointment SET appointmentDate=?,appointmentTime=?,appointmentDoctor=?,appointmentHospital=?WHERE appointmentID=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 			preparedStmt.setString(1, date);
